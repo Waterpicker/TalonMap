f271343dbd888b8489ef19ebce3563cd52fecef4
Waterpicker <waterpickerenternity@gmail.com>
2022-07-20 00:04:34 -0500

    Fixed the 'Received invalid biome id: -1' issue.

--- src/main/java/me/talonos/talonmap/world/ImageBiomeSource.java
+++ src/main/java/me/talonos/talonmap/world/ImageBiomeSource.java
@@ -6,56 +6,61 @@ import it.unimi.dsi.fastutil.ints.Int2ShortArrayMap;
 import it.unimi.dsi.fastutil.shorts.Short2ObjectArrayMap;
 import me.talonos.talonmap.lib.ImageUtil;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.dynamic.RegistryLookupCodec;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.PortalUtil;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BuiltinBiomes;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.source.BiomeSource;
 import org.apache.logging.log4j.LogManager;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class ImageBiomeSource extends BiomeSource {
+    private static final Codec<RegistryKey<Biome>> BIOME_KEY_CODEC = Identifier.CODEC.xmap(RegistryKey.createKeyFactory(Registry.BIOME_KEY), RegistryKey::getValue);
+
     public static final Codec<ImageBiomeSource> CODEC = RecordCodecBuilder.create(instance -> {
         return instance.group(
                 Codec.STRING.fieldOf("data").forGetter(a -> a.data),
-                Codec.list(Biome.REGISTRY_CODEC).fieldOf("mappings").forGetter(a -> a.mappings),
-                Biome.REGISTRY_CODEC.optionalFieldOf("filler", () -> BuiltinBiomes.PLAINS).forGetter(a -> () -> a.filler))
+                Codec.list(BIOME_KEY_CODEC).fieldOf("mappings").forGetter(a -> a.mappings),
+                BIOME_KEY_CODEC.optionalFieldOf("filler", BiomeKeys.THE_VOID).forGetter(a -> a.filler),
+                RegistryLookupCodec.of(Registry.BIOME_KEY).forGetter(vanillaLayeredBiomeSource -> vanillaLayeredBiomeSource.biomeRegistry))
                 .apply(instance, ImageBiomeSource::new);
     });
+    private final Biome biomeFiller;
     private String data;
-    private List<Supplier<Biome>> mappings;
-    private final Biome filler;
+    private List<RegistryKey<Biome>> mappings;
+    private final RegistryKey<Biome> filler;
 
     private short[][] biomeArray;
 
     public final Map<Short, Biome> shortToBiome = new Short2ObjectArrayMap<Biome>();
     private final Map<Integer, Short> colorToShort;
+    private Registry<Biome> biomeRegistry;
 
-    public ImageBiomeSource(String data, List<Supplier<Biome>> mappings, Supplier<Biome> filler) {
-        super(Stream.concat(mappings.stream(), Stream.of(filler)));
+    public ImageBiomeSource(String data, List<RegistryKey<Biome>> mappings, RegistryKey<Biome> filler, Registry<Biome> biomeRegistry) {
+        super(Stream.concat(mappings.stream(), Stream.of(filler)).map(registryKey -> () -> biomeRegistry.getOrThrow(registryKey)));
         this.data = data;
         this.mappings = mappings;
+        this.biomeRegistry = biomeRegistry;
 
-        this.filler = filler.get();
-
+        this.filler = filler;
+        this.biomeFiller = biomeRegistry.getOrThrow(filler);
         colorToShort = new Int2ShortArrayMap();
 
         short talonMapInternalBiomeId = 0;
 
         for (int i = 0, mappingSize = mappings.size(); i < mappingSize; i++) {
-            Supplier<Biome> m = mappings.get(i);
+            Biome m = biomeRegistry.getOrThrow(mappings.get(i));
             colorToShort.put(i, talonMapInternalBiomeId);
 //            LogManager.getLogger(Constants.MODID).debug(Biome.REGISTRY.getObject(new ResourceLocation(m.name)));
-            shortToBiome.put(talonMapInternalBiomeId, m.get());
+            shortToBiome.put(talonMapInternalBiomeId, m);
             talonMapInternalBiomeId++;
         }
 
@@ -80,7 +85,7 @@ public class ImageBiomeSource extends BiomeSource {
 
                 return shortToBiome.get(biomeArray[biomeX][biomeZ]);
             } else {
-                return filler;
+                return biomeFiller;
             }
     }
 
